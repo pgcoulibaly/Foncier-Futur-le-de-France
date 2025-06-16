7eaf875feca67defb9631c4731ea30b8a7cfaa6e
pcoulibaly <papagarbacoulibaly.com>
2025-06-16 15:01:47 +0200

    mise en place de l'api qui affiche les bien dans un rayon choisit et fait une analyse

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from fastapi.responses import JSONResponse
 import os
 from dotenv import load_dotenv
 from core.geocod import geocode_ban, haversine_distance
+from core.llm_assistant import analyse_biens_par_llm
 
 load_dotenv()
 
@@ -21,23 +22,16 @@ app.add_middleware(
 DATABASE_URL = os.getenv("NEON_DB_URL")
 engine = create_engine(DATABASE_URL)
 
-# Endpoint 1: Géocodage
 
-@app.get("/geocode_ban")
-def api_geocode_ban(adresse: str = Query(..., description="Adresse en France")):
-    lat, lon = geocode_ban(adresse)
-    if lat is None:
-        return JSONResponse(status_code=404, content={"error": "Adresse introuvable"})
-    return {"latitude": lat, "longitude": lon}
 
-# Endpoint 2: Biens proches
+# Endpoint : Biens proches
 @app.get("/biens_proches")
-def biens_proches(lat: float, lon: float, rayon_m: int = 500):
+def biens_proches(adresse: str = Query(..., description="Adresse en ile de France"), rayon_m: int = 500):
     query = text("""
-        SELECT latitude, longitude, prix_m2, type_local, date_mutation, surface_reelle_bati,id_mutation,nombre_pieces_principales
+        SELECT latitude, longitude, prix_m2, type_local, date_mutation, surface_reelle_bati, id_mutation, nombre_pieces_principales
         FROM valeurs_foncieres_idf_2024
     """)
-    
+    lat, lon = geocode_ban(adresse)
     with engine.connect() as conn:
         results = conn.execute(query)
         biens = []
@@ -50,11 +44,16 @@ def biens_proches(lat: float, lon: float, rayon_m: int = 500):
                     "prix_m2": row.prix_m2,
                     "type_local": row.type_local,
                     "date_mutation": row.date_mutation,
-                    "surface_reele_batie":row.surface_reelle_bati,
-                    "id_mutation":row.id_mutation,
-                    'nombre_pieces_principales':row.nombre_pieces_principales,
+                    "surface_reelle_bati": row.surface_reelle_bati,
+                    "id_mutation": row.id_mutation,
+                    "nombre_pieces_principales": row.nombre_pieces_principales,
                     "distance_m": d
                 })
-    return biens
+    
+    # Appel de ta fonction d'analyse importée
+    analyse = analyse_biens_par_llm(biens, rayon_m)
 
-
+    return {
+        "biens_proches": biens,
+        "analyse": analyse
+    }
