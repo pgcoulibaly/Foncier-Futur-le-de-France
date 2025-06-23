aa3a528aa07697c3ca161a80306e2a2f83758a1f
pcoulibaly <papagarbacoulibaly.com>
2025-06-23 02:17:03 +0200

    modification du gitignore

--- dataset_builder/dvf_ingestion_to_neon.py
+++ dataset_builder/dvf_ingestion_to_neon.py
@@ -3,13 +3,14 @@ import duckdb
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 # Récupérer l'URL depuis le .env
 neon_url = os.getenv("NEON_DB_URL")
 
 # Paramètres de base
-departements_idf = ['75', '92', '93', '94', '95', '78', '91', '77']
+departements_idf = ["75", "92", "93", "94", "95", "78", "91", "77"]
 annee = 2024
 base_url = f"https://files.data.gouv.fr/geo-dvf/latest/csv/{annee}/departements"
 
@@ -19,10 +20,10 @@ df_list = []
 for dep in departements_idf:
     url = f"{base_url}/{dep}.csv.gz"
     print(f"Chargement : {url}")
-    
+
     try:
-        df = pd.read_csv(url, sep=',', compression='gzip', low_memory=False)
-        df['departement'] = dep
+        df = pd.read_csv(url, sep=",", compression="gzip", low_memory=False)
+        df["departement"] = dep
         df_list.append(df)
     except Exception as e:
         print(f"Erreur lors du chargement du département {dep} : {e}")
@@ -32,40 +33,45 @@ df_idf = pd.concat(df_list, ignore_index=True)
 
 # Sélection des colonnes utiles
 features = [
-    'surface_reelle_bati',
-    'nombre_pieces_principales',
-    'type_local',
-    'code_postal',
-    'longitude',
-    'latitude',
-    'nature_mutation',
-    'date_mutation',
-    'valeur_fonciere',
-    'id_mutation',
-    'adresse_numero', 
-    'adresse_nom_voie',
+    "surface_reelle_bati",
+    "nombre_pieces_principales",
+    "type_local",
+    "code_postal",
+    "longitude",
+    "latitude",
+    "nature_mutation",
+    "date_mutation",
+    "valeur_fonciere",
+    "id_mutation",
+    "adresse_numero",
+    "adresse_nom_voie",
 ]
 
 df_idf = df_idf[features]
 
 # Filtrage : uniquement ventes de maisons et appartements
-query_1 = '''
+query_1 = """
     SELECT *
     FROM df_idf
     WHERE type_local IN ('Appartement', 'Maison') 
     AND nature_mutation = 'Vente'
-'''
+"""
 df_idf = duckdb.query(query_1).df()
 
 # Suppression des lignes avec valeurs manquantes
 df_idf.dropna(inplace=True)
-#création de la colonne adresse
-df_idf['adresse'] = df_idf['adresse_numero'].astype(int).astype(str).str.cat(df_idf['adresse_nom_voie'].str.lower(), sep=' ')
-#suppression des colonne 'adresse_numero', 'adresse_nom_voie'
-df_idf.drop(['adresse_numero', 'adresse_nom_voie'], axis=1, inplace=True)
+# création de la colonne adresse
+df_idf["adresse"] = (
+    df_idf["adresse_numero"]
+    .astype(int)
+    .astype(str)
+    .str.cat(df_idf["adresse_nom_voie"].str.lower(), sep=" ")
+)
+# suppression des colonne 'adresse_numero', 'adresse_nom_voie'
+df_idf.drop(["adresse_numero", "adresse_nom_voie"], axis=1, inplace=True)
 
 # Calcul du prix au mètre carré
-query_2 = '''
+query_2 = """
     WITH prix_par_m2 AS (
         SELECT 
             id_mutation, 
@@ -76,23 +82,21 @@ query_2 = '''
     SELECT df_idf.*, prix_par_m2.prix_m2
     FROM df_idf
     LEFT JOIN prix_par_m2 USING(id_mutation)
-'''
+"""
 df_idf = duckdb.query(query_2).df()
 
 # Filtrage : valeurs cohérentes du prix au m²
-query_3 = '''
+query_3 = """
     SELECT *
     FROM df_idf
     WHERE prix_m2 BETWEEN 1000 AND 25000
-'''
+"""
 df_idf = duckdb.query(query_3).df()
 
 # Suppression des colonnes devenues inutiles
-df_idf.drop(['valeur_fonciere', 'nature_mutation'], axis=1, inplace=True)
+df_idf.drop(["valeur_fonciere", "nature_mutation"], axis=1, inplace=True)
 
 print("Dataset final prêt :", df_idf.shape)
-
-
 
 
 # Colle ici l'URL obtenue depuis Neon
